711feea5af8a559e48ffd7a4583b872023ed9b46
unknown <abhinashm606@gmail.com>
2025-09-27 16:11:54 +0530

    first commit

--- first.cpp
+++ first.cpp
@@ -1,204 +1,238 @@
 # include <iostream>
 using namespace std;
 int main(){
-    int year;
-    cout<<"enter a year";
-    cin>>year;
+//     int year;
+//     cout<<"enter a year";
+//     cin>>year;
 
-    if((year%4==0 && year%100!=0 ) || (year%400==0))
-    {
-        cout<<"yes";
-    }
+//     if((year%4==0 && year%100!=0 ) || (year%400==0))
+//     {
+//         cout<<"yes";
+//     }
 
-    else if(year%400==0)
-    {
-        cout<<"century";
-    }
+//     else if(year%400==0)
+//     {
+//         cout<<"century";
+//     }
 
-    else{
-        cout<<"no";
-    }
+//     else{
+//         cout<<"no";
+//     }
    
-// meter reading question
+// // meter reading question
 
-    int unit;
-    cout<<"enter unit";
-    cin>>unit;
+//     int unit;
+//     cout<<"enter unit";
+//     cin>>unit;
 
-    if(unit>0 && unit<=50) cout<<unit*1;
-    else if(unit>50 && unit<=150) cout<<50*1+(unit-50)*2;
-    else if(unit>150 &&  unit<=250) cout<<50*1+100*2+(unit-150)*3;
-    else if(unit>250) cout<<50*1+100*2+100*3+(unit-250)*4;
+//     if(unit>0 && unit<=50) cout<<unit*1;
+//     else if(unit>50 && unit<=150) cout<<50*1+(unit-50)*2;
+//     else if(unit>150 &&  unit<=250) cout<<50*1+100*2+(unit-150)*3;
+//     else if(unit>250) cout<<50*1+100*2+100*3+(unit-250)*4;
 
-// swaping
+// // swaping
 
-    int a=65;
-    int b=41;
+//     int a=65;
+//     int b=41;
 
-    int temp;
+//     int temp;
 
-    temp=a;
-    a=b;
-    a=temp;
+//     temp=a;
+//     a=b;
+//     a=temp;
 
-    cout<<a<<b;
+//     cout<<a<<b;
     
-    int a=23;
-    int b=34;
+//     int a=23;
+//     int b=34;
 
-    a=a+b;
-    b=a-b;
-    a=a-b;
+//     a=a+b;
+//     b=a-b;
+//     a=a-b;
 
-    cout<<a<<b;
+//     cout<<a<<b;
 
-    int option;
-    cout<<"enter a option";
-    cin>>option;
+//     int option;
+//     cout<<"enter a option";
+//     cin>>option;
 
-    if(option==1)
-    {
-        int a=23;
-        int b=45;
+//     if(option==1)
+//     {
+//         int a=23;
+//         int b=45;
 
-        a=a+b;
-        b=a-b; 
-        a=a-b;
-        cout<<a<<b;
-    }
+//         a=a+b;
+//         b=a-b; 
+//         a=a-b;
+//         cout<<a<<b;
+//     }
 
-else if(option)
+// else if(option)
+// {
+//     int a=45;
+//     int b=1;
+//     int c=34;
+//   }
+
+    
+// int user;
+// cout<<"enter a user";
+// cin>>user;
+
+// if(user==1){
+//     cout<<"veg";
+// }
+
+// else if(user==2){
+//     cout<<"non veg";
+
+// }
+
+// else{
+//     cout<<"desert";
+// }
+
+// int user;
+// cout<<"enter a user";
+//  cin>>user;
+// switch (user){
+//     case1:cout<<"veg";
+//     case2:cout<<"non veg";
+// }
+    
+
+
+
+// // disadvatages-
+// // position is deafault is no fix
+// // only two type of data type-int ,char
+// // opertor are not allowed-&& or not
+
+    
+//   //lopp
+// int i=1;
+// while(i<=10);
+// {
+//     cout<<i++;
+// }
+
+// for(int i=1; i<=10;++i )
+// {
+//     cout<<"2*"<<i<<"="<<2*i<<endl;
+    
+
+// }
+
+// int n1=18;
+// int n2=7;
+// int hcf=0;
+// int lcm=0;
+
+// for(int i=n1; ;i=i+n1)
+// {
+//     if(i%n1==0 && i%n2==0)
+//     {
+//         cout<<i;
+//         break;
+//     }
+// }
+
+
+// //prime number
+
+// int n=17;
+// int count=0;
+// for(int i=1;i<=n;++i)
+// {
+// if(n%i==0)
+// {
+//     ++count;
+// } 
+//    }
+//    if(count==2)   cout<<"prime";
+//    else cout<<"not prime";
+
+
+// //perfact number
+
+// int n=7;
+// int sum=0;
+// for(int i=1;i<n;++i)
+// {
+//     if(n%i==0)
+// {
+// sum=sum+i;
+// }
+
+// }
+
+// if(sum==n)  cout<<"perfact";
+
+
+// //pallindrom
+
+// int number=121;
+// int org=number;
+// int rev=0;
+
+// while(number!=0)
+// {
+//   int digit=number%10;
+//   rev=rev*10+digit;
+//   number=number/10;
+// }
+// if(rev==org) cout<<"pallindrom";
+// else cout<<"not pallindrom";
+
+// //armstrong number
+
+// int number=153;
+// int org=number;
+// int sum=0;
+
+// while(number!=0)
+// {
+//   int digit=number%10;
+//   sum=sum +digit*digit*digit;
+//   number=number/10;
+// }
+// if(sum==org) cout<<"armstrong";
+// else cout<<"not armstrong";
+
+int temp;
+cout<<"temprature in centigrate";
+cin>>temp;
+
+
+if(temp<0)
 {
-    int a=45;
-    int b=1;
-    int c=34;
-  }
-
-    
-int user;
-cout<<"enter a user";
-cin>>user;
-
-if(user==1){
-    cout<<"veg";
+    cout<<"frezing weather"<< endl;
 }
 
-else if(user==2){
-    cout<<"non veg";
+else if(temp>=0 && temp<10)
+{
+    cout<<"very cold weather"<<endl;
+}
 
+else if(temp>=10 && temp<20)
+{
+   cout<<"cold weather"<<endl;
+}
+
+else if(temp>=20 && temp<30)
+{
+    cout<<"the normal temp"<<endl;
+}
+
+else if(temp>=30 && temp<40)
+{
+    cout<<"then its hot"<<endl;
 }
 
 else{
-    cout<<"desert";
-}
-
-int user;
-cout<<"enter a user";
- cin>>user;
-switch (user){
-    case1:cout<<"veg";
-    case2:cout<<"non veg";
+    cout<<"then its very hot"<<endl;
 }
     
-
-
-
-// disadvatages-
-// position is deafault is no fix
-// only two type of data type-int ,char
-// opertor are not allowed-&& or not
-
-    
-  //lopp
-int i=1;
-while(i<=10);
-{
-    cout<<i++;
-}
-
-for(int i=1; i<=10;++i )
-{
-    cout<<"2*"<<i<<"="<<2*i<<endl;
-    
-
-}
-
-int n1=18;
-int n2=7;
-int hcf=0;
-int lcm=0;
-
-for(int i=n1; ;i=i+n1)
-{
-    if(i%n1==0 && i%n2==0)
-    {
-        cout<<i;
-        break;
-    }
-}
-
-
-//prime number
-
-int n=17;
-int count=0;
-for(int i=1;i<=n;++i)
-{
-if(n%i==0)
-{
-    ++count;
-} 
-   }
-   if(count==2)   cout<<"prime";
-   else cout<<"not prime";
-
-
-//perfact number
-
-int n=7;
-int sum=0;
-for(int i=1;i<n;++i)
-{
-    if(n%i==0)
-{
-sum=sum+i;
-}
-
-}
-
-if(sum==n)  cout<<"perfact";
-
-
-//pallindrom
-
-int number=121;
-int org=number;
-int rev=0;
-
-while(number!=0)
-{
-  int digit=number%10;
-  rev=rev*10+digit;
-  number=number/10;
-}
-if(rev==org) cout<<"pallindrom";
-else cout<<"not pallindrom";
-
-//armstrong number
-
-int number=153;
-int org=number;
-int sum=0;
-
-while(number!=0)
-{
-  int digit=number%10;
-  sum=sum +digit*digit*digit;
-  number=number/10;
-}
-if(sum==org) cout<<"armstrong";
-else cout<<"not armstrong";
-
 
 
 
